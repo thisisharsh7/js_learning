c709700c0de336e71eb84e1fcf0fa5ab4dd6dec7
Harsh Kumar <9u.harsh@gmail.com>
2022-06-25 09:25:45 +0530

    some code added

--- learning1/javascript/index6.js
+++ learning1/javascript/index6.js
@@ -1,17 +1,42 @@
 // console.log("Here comes");
+// let arr=new Array(23,123,'orange');
+// console.log(arr);
 // // let ar=[1,3,5];
 // let ar2=['harsh','ram','vasu'];
 // console.log(ar);
+// let value=ar2.indexOf('harsh');
+// console.log(value);//output index of 'harsh'
 // console.log(ar2);
-// ar.push(2);
-// ar.unshift(23);
+//mutating or modifying array
+// ar.push(2);//add at end
+// ar.unshift(23);//add at first
 // console.log(ar);
-// ar.shift();
-// ar.splice(1,2);
-// ar.reverse();
-// ar.concat(arr2);
+//ar.pop()//remove from end
+// ar.shift();//remove from first
+// ar.splice(1,2); start from index 1 and remove 2 elements
+// ar.reverse();//reversing the arr
+// ar.concat(arr2);//ar will containe its and arr2 element
+/*
+let myObject={
+    name : 'harsh';
+    channel : 'code';
+    isActive : 'false';
+    marks : [1,3,4,5]
+}
+for(let key in myObject){
+    console.log(`The ${key} of Object is ${myObject[key]});
+}
+console.log(myObject);
+console.log(myObject.name);
+console.log(myObject.channel);
+or we can access elements by 
+console.log(myObject['isActive']);
+ar2.forEach(function(element){
+    console.log(elelment);you can traverse every element of the array.
+})
+*/
 // ar2.forEach(function(element,index,array){
-//     console.log(element,index,array);
+//     console.log(element,index,array);element index and full array will be in each output line
 // })
 // ar2.forEach(function(element,index,array){
 //     console.log(element,index,array);
--- learning1/javascript/index7.js
+++ learning1/javascript/index7.js
@@ -11,16 +11,27 @@
 // function greet(name){
 //     console.log(`happy birthday ${name}`);
 // }
+/*
+function greet(name,thank="thank you!"){
+    console.log(`happy birthday ${name} and 
+                 ${thank}.`);
+}
+greet('harsh');
+greet('harsh','thanks a lot');
+
+*/
 // let nam3='skillF';
 // let nam2='rohan';
 // console.log(`Happy birthday ${nam3}`);
 // console.log(`happy birthday ${nam2}`);
-// greet(nam2);
-// greet(nam3);
+// greet(nam2);//function call
+// greet(nam3);//function call
 // const myObj={
 //     name: "why",
-//     game: function(){
+//     game: function(){//anonymous function function with no name
 //         return "GTA";
 //     }
 // }
 // console.log(myObj.game());
+/* let has block level scope
+and var has function level scope*/
